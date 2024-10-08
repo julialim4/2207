2228159ce735154e992e650c8689c03be48af4bb
julialim4 <161314817+julialim4@users.noreply.github.com>
2024-10-08 12:24:00 -0300

    vbvb

---
+++ perguntas.js
@@ -0,0 +1,128 @@
+criaCartao(
+    'matematica' , 
+    'como se calcula a soma dos termos de uma P.A?',
+    'utiliza-se a formula; ((a1=an)).q)/2'
+)
+
+criaCartao(
+    'matematica' , 
+    'qual a formula da hipotenuza?'
+    'c2=a2+b2'
+)
+
+ criaCartao(
+    'fisica'
+    'quia sao os tres estados da matéria?'
+    'solido, líquido e gasoso'
+ )
+
+criaCartao(
+'biologia'
+'o que é a fotossíntese?'
+'e´um precesso pelo qual as plantas convvertem luz solar em energia'
+)
+
+criaCartao(
+'quimica'
+'qual a fórmula da água?'
+'H2O'
+)
+
+criaCartao(
+'quimica'
+'O que é uma reação quimica?'
+'é um processo que transforma substâncias, inclui reagentes e produtos'
+)
+
+criaCartao(
+   'história'
+    'Causas da Primeira Guerra?'
+    'Nacionalismo, imperialismo, militarismo, alianças'
+
+)
+criaCartao( 
+    'história'
+'O que foi a Revolução Industrial?'
+'Transição para produção mecanizada; urbanização e mudanças sociais'
+)
+
+criaCartao(
+    'história'
+    'Quem foi Dom Pedro I?
+    'Primeiro imperador do Brasil, proclamou a independência em 1822'
+)
+
+criaCartao(
+    'português'
+'Quais são os tipos de texto?'
+'Narrativo, descritivo, dissertativo, injuntivo.'
+)
+
+criaCartao(
+    'português'
+'Figuras de linguagem?'
+'Recursos que enriquecem o texto, como metáfora e aliteração.'
+)
+
+criaCartao(
+    'português'
+'Construção de argumentação?'
+'Tese clara, evidências e refutação de contra-argumentos.'
+)
+
+criaCartao(
+    'inglês'
+' O que é o Present perfect?'
+'Ações passadas relevantes no presente. Estrutura: "have/has + past participle"'
+)
+
+criaCartao(
+    'inglês'
+    'Quais são os principais tempos verbais em inglês e suas utilizações?'
+    'Present simple, past simple, present continuous, future simple.'
+)
+
+
+
+
+criaCartao(
+''
+    'Qual é a estrutura básica da célula?'
+
+)
+
+criaCartao(
+    'biologia'
+    'O que é a fotossíntese e qual é sua importância?'
+)
+
+criaCartao(
+    'biologia'
+    'Quais são os principais sistemas do corpo humano e suas funções?'
+
+
+)
+
+
+
+
+criaCartao(
+
+
+
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
