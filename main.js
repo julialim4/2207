96502a2eb7b0b8e2da2fdc947042a9fb47fad07b
julialim4 <161314817+julialim4@users.noreply.github.com>
2024-10-15 11:43:11 -0300

    kbmmb

---
+++ main.js
@@ -0,0 +1,26 @@
+ function criaCartao (categoria, pergunta,resposta){
+  let container =document.getElementById('container');
+  let cartao = document.createElement('article');
+  cartao.className = 'cartao';
+  cartao.innerHTML=`
+  
+  <div class="conteudo-cartao">
+    <h3>${categoria}</h3>
+    <div class="pertunta-cartao">
+    <p>${pergunta}</p>
+     </div>
+     <div class="reposta-cartao">
+    <p>${reposta}</p>
+     </div>
+</div>
+`
+container.appendChild(cartao)
+}
+  
+  
+
+
+
+ 
+
+
