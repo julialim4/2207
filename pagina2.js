c41d58ce452f0bb64775687c891413d31766d09b
julialim4 <161314817+julialim4@users.noreply.github.com>
2024-10-30 12:23:54 -0300

    fgxhdfg

---
+++ pagina2.js
@@ -0,0 +1,20 @@
+function criaCartao (categoria, pergunta,resposta){
+    let container =document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML=`
+    
+    <div class="conteudo-cartao">
+      <h3>${categoria}</h3>
+      <div class="pertunta-cartao">
+      <p>${pergunta}</p>
+       </div>
+       <div class="reposta-cartao">
+      <p>${resposta}</p>
+       </div>
+  </div>
+  `
+  
+  container.appendChild(cartao);
+   }
+  
