20032a1d530fb1d134ff7c938d274a844b9c4e98
julialim4 <161314817+julialim4@users.noreply.github.com>
2024-10-30 12:09:05 -0300

    xsx

--- main.js
+++ main.js
@@ -14,13 +14,15 @@
      </div>
 </div>
 `
-container.appendChild(cartao)
+
+let respostaEstaVisivel = false
+
+function viraCartao(){
+   respostaEstaVisivel = !respostaEstaVisivel;
+   cartao.classList.toggle ('active', respostaEstaVisivel)
 }
-  
-  
 
+cartao.addEventListener('click' , viraCartao);
 
-
- 
-
-
+container.appendChild(cartao);
+ }
